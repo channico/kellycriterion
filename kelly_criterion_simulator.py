f88a078a44049914d9c52719cabcf6b3f16b9dca
Nicodemus Chan <nicodemus@phonester.com>
2026-08-14 08:07:09 -0400

    Add in random seed input and clean up method calls

--- kelly_criterion_simulator.py
+++ kelly_criterion_simulator.py
@@ -4,6 +4,7 @@
 
 # %%
 import numpy as np
+import secrets
 
 # Function to calculate the optimal bet fraction using Kelly Criterion
 def kelly_criterion(win_rate, win_payoff, lose_payoff):
@@ -12,14 +13,15 @@ def kelly_criterion(win_rate, win_payoff, lose_payoff):
                )
 
 
-def simulate_kelly(win_rate, win_payoff, lose_payoff, initial_capital, num_bets, kelly_fraction):
+def simulate_kelly(win_rate, win_payoff, lose_payoff, initial_capital, num_bets, kelly_fraction, seed=None):
     bet_fraction = kelly_criterion(win_rate, win_payoff, lose_payoff) * kelly_fraction
     capital = initial_capital
     capital_history = [capital]
     streak = {'win': 0, 'lose': 0}
     current_streak = {'win': 0, 'lose': 0}
 
-    rng = np.random.default_rng(seed=None)
+    used_seed = secrets.randbits(64) if seed is None else seed
+    rng = np.random.default_rng(used_seed)
     outcomes = (rng.random(num_bets) < win_rate)
 
     for outcome in outcomes:
@@ -36,4 +38,10 @@ def simulate_kelly(win_rate, win_payoff, lose_payoff, initial_capital, num_bets,
         streak['win'] = max(streak['win'], current_streak['win'])
         streak['lose'] = max(streak['lose'], current_streak['lose'])
 
-    return capital_history, streak['win'], streak['lose'], bet_fraction
+    return (
+        capital_history,
+        streak['win'],
+        streak['lose'],
+        bet_fraction,
+        used_seed
+    )
